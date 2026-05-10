24d8acac161b972751b581fc406b6528cc4b54ad
martkasobie <76697429+martkasobie@users.noreply.github.com>
2026-05-10 16:28:48 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'v22'; // <--- WERSJA 22: Wdrożono "Cichy Patrol" podtrzymujący status Aktywnego Obywatela!
+const SW_VERSION = 'v23'; // <--- WERSJA 23: Ostateczna dekapitacja biurokracji! Wymuszamy odświeżenie plików!
 
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
