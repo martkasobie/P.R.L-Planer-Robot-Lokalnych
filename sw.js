911bfc3ea7acd84e121b88a82f05e9ee6db1663d
martkasobie <76697429+martkasobie@users.noreply.github.com>
2026-07-18 15:17:35 +0200

    Bump service worker version to v74
    Updated service worker version to enforce cache update.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'v73'; // <--- Podbita wersja, wymuszamy aktualizację Magazynu!
+const SW_VERSION = 'v74'; // <--- Podbita wersja, wymuszamy aktualizację Magazynu!
 const CACHE_NAME = 'prl-magazyn-' + SW_VERSION;
 
 // Podstawowe akta, które muszą być na stałe w szufladzie
