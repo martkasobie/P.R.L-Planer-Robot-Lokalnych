843eba4e35162d9765abb18f5d79a124e5b8e015
martkasobie <76697429+martkasobie@users.noreply.github.com>
2026-09-23 20:17:06 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'v121'; // <--- Podbita wersja, wymuszamy aktualizację Magazynu!
+const SW_VERSION = 'v122'; // <--- Podbita wersja, wymuszamy aktualizację Magazynu!
 const CACHE_NAME = 'prl-magazyn-' + SW_VERSION;
 
 // Podstawowe akta, które muszą być na stałe w szufladzie
